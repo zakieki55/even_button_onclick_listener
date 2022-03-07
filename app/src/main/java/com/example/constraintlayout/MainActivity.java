14cfb7c52cc7d8313033ddbc692963f48d27812d
zakieki55 <zakueki@gmail.com>
2022-03-07 22:28:54 +0700

    second commit

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.constraintlayout;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btSignin);
+        edemail = findViewById(R.id.edEmail);
+        edpassword = findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                String email = "admin";
+                String pass="eki";
+//                  activity 1
+//                Toast t = Toast.makeText(getApplicationContext(),
+//                        "email anda: "+ nama+" dan password anda: "+password+"", Toast.LENGTH_LONG);
+//                t.show();
+
+                if(nama.isEmpty()|| password.isEmpty()){
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan Password anda wajib diisi!!!",
+                    Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else{
+                    if(nama.equals(email) && password.equals(pass)){
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses",
+                                Toast.LENGTH_LONG);
+
+                        t.show();
+
+                        Bundle b = new Bundle();
+
+                        b.putString("a", nama.trim());
+
+                        b.putString("b", password.trim());
+
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        i.putExtras(b);
+
+                        startActivity(i);
+                    }
+                    else{
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login gagal", Toast.LENGTH_LONG);
+
+                        t.show();
+                    }
+                }
+            }
+        });
     }
+
+
 }
