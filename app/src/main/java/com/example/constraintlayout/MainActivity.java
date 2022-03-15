44e3e9a34b576cb2c032e3128103dd9c184f69a0
zakieki55 <zakueki@gmail.com>
2022-03-15 14:46:57 +0700

    third commit

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.constraintlayout;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,24 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.mnDaftar){
+            Intent i = new Intent(getApplicationContext(), ActivityDaftar.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     EditText edemail, edpassword;
     String nama, password;
 
