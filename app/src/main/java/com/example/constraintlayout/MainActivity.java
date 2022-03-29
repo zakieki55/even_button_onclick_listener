d0f47792e8f453ed4122ef8c94670077139eb72c
zakieki55 <zakueki@gmail.com>
2022-03-29 15:19:57 +0700

    PopUp Menu dan ListView

--- app/src/main/java/com/example/constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
                         b.putString("b", password.trim());
 
-                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+                        Intent i = new Intent(getApplicationContext(), Home_Activity.class);
 
                         i.putExtras(b);
 
